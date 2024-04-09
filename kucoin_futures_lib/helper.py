bfc4aac633275bd95abcbc5a7f1f412e0a8e2016
Evgeny Aleshin <aleshiner@gmail.com>
2024-04-09 15:45:11 +0100

    fix calculate lots method

--- kucoin_futures_lib/helper.py
+++ kucoin_futures_lib/helper.py
@@ -2,6 +2,7 @@
 
 import logging
 import math
+from decimal import Decimal
 
 logger = logging.getLogger(__name__)
 
@@ -11,12 +12,12 @@ class KucoinFuturesHelper:
 
     @staticmethod
     def calculate_lots(
-        investment_amount: float, current_price: float, lot_size: float
+        lot_size: float, current_price: float, investment_amount: float,
     ) -> int:
         """Calculate the maximum number of lots to buy based on the investment amount, current price, and lot size.
-        :param investment_amount: The maximum amount to invest.
-        :param current_price: The current price of the instrument.
         :param lot_size: The lot size for the instrument. E.g. 0.001 for BTC/USDT.
+        :param current_price: The current price of the instrument.
+        :param investment_amount: The maximum amount to invest.
         :return: The maximum number of lots to buy.
         """
         logger.debug(
@@ -25,8 +26,16 @@ class KucoinFuturesHelper:
             current_price,
             lot_size,
         )
+
+        if lot_size == 0 or current_price == 0 or investment_amount == 0:
+            return 0
+
+        current_price = Decimal(str(current_price))
+        investment_amount = Decimal(str(investment_amount))
+        lot_size = Decimal(str(lot_size))
+
         lot_price = current_price * lot_size
-        lots = investment_amount // lot_price
+        lots = (investment_amount / lot_price).to_integral_value(rounding='ROUND_FLOOR')  # Use Decimal's floor rounding
         return int(lots)
 
     @staticmethod
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -17,3 +17,24 @@ def test_calculate_precision(tick_size, expected_precision):
     tick_size = 0.1
     precision = KucoinFuturesHelper.calculate_precision(tick_size=tick_size)
     assert precision == 1
+
+
+@pytest.mark.parametrize(
+    "investment_amount, current_price, lot_size, expected_lots",
+    [
+        (1000, 10, 0.1, 1000),  # Normal case
+        (1000, 10, 0, 0),  # Lot size is zero
+        (1000, 0, 0.1, 0),  # Current price is zero
+        (0, 10, 0.1, 0),  # Investment amount is zero
+        (1000, 10, 1, 100),  # Lot size is one
+        (1000, 1, 0.1, 10000),  # Current price is one
+        (1, 10, 0.1, 1),  # Investment amount is one
+    ],
+)
+def test_calculate_lots(investment_amount, current_price, lot_size, expected_lots):
+    lots = KucoinFuturesHelper.calculate_lots(
+        investment_amount=investment_amount,
+        current_price=current_price,
+        lot_size=lot_size
+    )
+    assert lots == expected_lots
